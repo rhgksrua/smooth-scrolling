f397b1cb284dfb651bf65171126e7c40fcba0608
Han Koh <rhgksrua@gmail.com>
2015-06-07 16:45:27 -0500

    added easeinout and comments

--- README.md
+++ README.md
@@ -3,6 +3,3 @@
 WIP
 
 ### TODO
-
-* Add ease in out function for scroll movement.
-* Page scrolls more than it needs to.  Part of first element is hidden.  Moves too far down.
--- scroll.js
+++ scroll.js
@@ -3,26 +3,47 @@ document.addEventListener('DOMContentLoaded', function() {
 	console.log(links);
 	var i, length;
 
+	/**
+	 * Quadratic ease-in function.  This function can be found just by Googling.
+	 * @param  {number} currentTime Amount of time passed
+	 * @param  {number} startValue  Start position
+	 * @param  {number} change      Current position of the window
+	 * @param  {number} duration    Total time it takes to complete the scroll
+	 * @return {number}             New location for scrollTop
+	 */
+	var quadEaseIn = function (currentTime, startValue, change, duration) {
+		currentTime /= duration/2;
+		if (currentTime < 1) return change/2 * currentTime * currentTime + currentTime;
+		currentTime--;
+		return -change/2 * (currentTime * (currentTime - 2) - 1) + currentTime;
+	};
+
+	/**
+	 * smoothScroll function is attached to the anchors
+	 * @param  {} e event
+	 * @return {}   
+	 */
 	var smoothScroll = function(e) {
 		e.preventDefault();
 		var startTime;
 		var currentPos;
 		var startPos = document.body.scrollTop;
+		// anchor href is matched to element with the same id without '#'.
 		var id = e.target.href.split('#')[1];
+		// Top position of the target element.
 		var endPos = document.getElementById(id).getBoundingClientRect().top;
 		var maxScroll = document.body.scrollHeight - window.innerHeight;
 		var scrollEndValue = startPos + endPos < maxScroll ? endPos: maxScroll - startPos;
-		var duration = 900;
+		// Time it takes to reach the target element.
+		// It is set to endPos.  Takes shorter time to reach closer elements.
+		var duration = endPos;
 		var scroll = function(timestamp) {
 			startTime = startTime || timestamp;
 			currentPos = document.body.scrollTop;
 			var elapsedTime = timestamp - startTime;
-			document.body.scrollTop = document.body.scrollTop + 10;
-			console.log(currentPos, endPos);
-			if ((window.innerHeight + document.body.scrollTop) >= document.body.offsetHeight) {
-				return false;
-			}
-			currentPos < endPos && requestAnimationFrame(scroll);
+			// Updates scroll position
+			document.body.scrollTop = quadEaseIn(elapsedTime, startPos, scrollEndValue, duration);
+			elapsedTime < duration && requestAnimationFrame(scroll);
 		}
 		requestAnimationFrame(scroll);	
 	}
